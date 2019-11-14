21271fb7b414cac325f2db076f3b181de56fef13
Anthony <anthony@premierlegalmarketing.com>
2019-11-14 16:23:27 -0500

    Modified README, Added Files

---
+++ gforms_wp_blacklist_filter.php
@@ -0,0 +1,37 @@
+/*Gravity Forms Filter to Use WP 'Comment Blacklist' as Validation Parameter*/
+add_filter( 'gform_validation', 'custom_validation' );
+function custom_validation( $validation_result ) {
+    
+	$blacklisted = get_option('blacklist_keys');
+	//Turn WP Blacklist Words into Array
+	$badWords = preg_split("/[\s,]+/", $blacklisted);
+	//Function to Check Submitted Field Value Against Blacklist Array. Will be Called in the Fields Loop.
+	function contains($field_value, array $badWords){
+		foreach($badWords as $i) {
+			if (stripos($field_value,$i) !== false)return true;
+		}
+			return false;
+	}
+
+    $form = $validation_result['form'];
+        //Loop through Form Fields
+        foreach( $form['fields'] as &$field ) {
+            //Store Submitted Field Value in a Local Variable
+			$field_value = rgpost( "input_{$field['id']}" );
+			//Use Function
+            if (contains($field_value, $badWords)){
+				// set the form validation to false
+				$validation_result['is_valid'] = false;
+                $field->failed_validation = true;
+                $field->validation_message = 'This field contains bad words!';
+                break;
+			}
+			else {
+				continue;
+			}
+        }
+ 
+    //Assign modified $form object back to the validation result
+    $validation_result['form'] = $form;
+    return $validation_result;
+}
